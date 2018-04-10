6b319a87eea19ed2a9002bf51027afe1e0bd83d8
gusmattos1 <gusmattos@gmail.com>
2018-04-10 15:29:09 -0400

    Starting assignment 2

---
+++ assignment2/crm.rb
@@ -0,0 +1,41 @@
+require 'sinatra'
+require_relative 'contact'
+
+get '/' do
+  redirect to ('/home')
+end
+
+get '/home' do
+  @title = 'Home'
+  erb :index
+end
+
+
+get '/contacts' do
+  @title = 'Contacts'
+  @contacts = Contact.all
+  erb :contacts
+end
+
+get '/contacts/:id' do
+  @contact = Contact.find_by({id: params[:id].to_i})
+  if @contact
+    erb :show_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+get '/new' do
+  erb :new
+end
+
+get '/about' do
+  @title = 'about'
+
+  erb :about
+end
+
+after do
+  ActiveRecord::Base.connection.close
+end
